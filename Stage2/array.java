60f891d560f8eb2ebf346f57ef823197c5fae7d2
MewPK <milzaza123@gmail.com>
2022-12-24 23:29:16 +0700

    Array!

--- Stage2/array.java
+++ Stage2/array.java
@@ -1,5 +1,23 @@
 package Stage2;
 
+import java.util.Arrays;
+
 class array {
+    public static void main(String[] args) {
+        int [] setNumber = new int[20];
+        for (int i = 0; i < 20; i++) {
+            setNumber[i] = i;
+        }
+        System.out.println(Arrays.toString(setNumber));
+
+        String [] animals = {"Cat" , "Dog" , "Fish"};
+        System.out.println(Arrays.toString(animals));
+        
+
+        int [][] array2d = {{0,1} , {0,1}};
+
+        System.out.println(Arrays.deepToString(array2d));
+    }
+    
     
 }
