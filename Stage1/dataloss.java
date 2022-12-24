66b83f51ce97d9ac8e74aeb1450b96e876b6f1a7
MewPK <milzaza123@gmail.com>
2022-12-24 22:13:54 +0700

    Variable and Dataloss in Java

---
+++ Stage1/dataloss.java
@@ -0,0 +1,17 @@
+public class dataloss {
+    public static void main(String[] args) {
+        // small --> big
+        int number1 = 500;
+        double number2 = number1;
+
+        // big --> small
+        double number3 = 123.456;
+        int number4 = (int)number3;
+
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(number4);
+        
+    }
+}
---
+++ Stage1/variable.java
@@ -0,0 +1,24 @@
+public class variable {
+    public static void main(String[] args) {
+        final int number1;
+        float number2;
+        double number3;
+        boolean status;
+        char character;
+        String strings;
+
+        number1 = 100;
+        number2 = 0.12f;
+        number3 = 0.123421f;
+        status = true;
+        character = 'A';
+        strings = "MewPK";
+        System.out.println(number1);
+        System.out.println(number2);
+        System.out.println(number3);
+        System.out.println(status);
+        System.out.println(character);
+        System.out.println(strings);
+        
+    }
+}
