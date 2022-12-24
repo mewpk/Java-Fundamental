5c30883522dd2c7a133c4e4f69a90635f509c37c
MewPK <milzaza123@gmail.com>
2022-12-24 23:07:44 +0700

    successful Stage1!

--- Stage1/input.java
+++ Stage1/input.java
@@ -1,11 +1,12 @@
 import java.util.Scanner;
 public class input {
  public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
-    String name = sc.nextLine();
-    System.out.println(name);
-    int number1 = sc.nextInt();
-    System.out.println(number1);
-    System.out.println(number1+name);
+    try (Scanner sc = new Scanner(System.in)) {
+        String name = sc.nextLine();
+        System.out.println(name);
+        int number1 = sc.nextInt();
+        System.out.println(number1);
+        System.out.println(number1+name);
+    }
  }   
 }
