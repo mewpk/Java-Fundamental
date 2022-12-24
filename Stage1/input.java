143d2b1ed4a2e9701f4f8311a2de169a609cffbe
MewPK <milzaza123@gmail.com>
2022-12-24 23:05:01 +0700

    input!

--- Stage1/input.java
+++ Stage1/input.java
@@ -4,6 +4,8 @@ public class input {
     Scanner sc = new Scanner(System.in);
     String name = sc.nextLine();
     System.out.println(name);
-    
+    int number1 = sc.nextInt();
+    System.out.println(number1);
+    System.out.println(number1+name);
  }   
 }
